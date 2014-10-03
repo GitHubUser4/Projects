65674c9cef42716efd8f3d4d2d6be0da54e15517
GitHubUser4 <3adamant@mail.ru>
2014-10-03 12:54:11 +0400

    123

--- Threads/src/TwoThreadsDemo.java
+++ Threads/src/TwoThreadsDemo.java
@@ -1,6 +1,8 @@
 public class TwoThreadsDemo {
-    public static void main (String[] args) {
-        new ExampleThread("Do it!").start();
-        new ExampleThread("Definitely not!").start();
+    public static void main(String[] args) {
+        new ExampleThread("Thread 1").start();
+        new ExampleThread("Thread 2").start();
+        new ExampleThread("Thread 3").start();
+        new ExampleThread("Thread 4").start();
     }
 }
